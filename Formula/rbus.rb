919551dedff63ec6519a294b7de1a5d78b3f9665
Robert Stephens <robert64@bellsouth.net>
2025-06-24 14:03:03 -0400

    update

--- Formula/rbus.rb
+++ Formula/rbus.rb
@@ -1,68 +1,66 @@
 class Rbus < Formula
-  desc "RDK-Bus (RBUS) messaging framework"
-  homepage "https://github.com/rdkcentral/rbus"
-  url "https://github.com/stepherg/rbus/archive/47c405dc4aea747a7af1e568586420e8aa5510dd.tar.gz"
-  version "2.3.1"
-  sha256 "413adc6d8e757e883285040c479e628348a6e371fb0611460f6b748c92b7d776"
-  license "Apache-2.0"
-
-  depends_on "cmake" => :build
-  depends_on "cjson"
-  depends_on "msgpack-c"
-  depends_on "stepherg/tap/linenoise"
-
-  def install
-    # Configure CMake with Homebrew dependencies
-    args = std_cmake_args + %W[
-      -DBUILD_FOR_DESKTOP=OFF
-      -DBUILD_RBUS_DAEMON=ON
-      -DBUILD_RBUS_SAMPLE_APPS=OFF
-      -DBUILD_RBUS_TEST_APPS=OFF
-      -DBUILD_ONLY_RTMESSAGE=OFF
-      -DENABLE_RDKLOGGER=OFF
-      -DRDKC_BUILD=OFF
-      -DWITH_SPAKE2=OFF
-      -DMSG_ROUNDTRIP_TIME=OFF
-      -DENABLE_UNIT_TESTING=OFF
-      -DCMAKE_POLICY_VERSION_MINIMUM=3.5
-    ]
-    mkdir "build" do
-      system "cmake", "..", *args
-      system "make"
-      system "make", "install"
-    end
-    # Create log directory
-    (var/"log/rbus").mkpath
-  end
-
-  service do
-    run [opt_bin/"rtrouted"]
-    run_type :immediate
-    keep_alive false
-    log_path var/"log/rbus/rtrouted.log"
-    error_log_path var/"log/rbus/rtrouted.err"
-    process_type :background
-  end
-
-  def caveats
-    <<~EOS
-      To start rbus now and restart at login:
-        brew services start rbus
-      Or, if you don't want/need a background service, you can run:
-        #{opt_bin}/rtrouted
-
-      Optional arguments for rtrouted can be configured manually by editing:
-        #{etc}/rbus/rtrouted.conf
-      and passing them via a wrapper script or custom launchd plist.
-
-      Logs are written to:
-        #{var}/log/rbus/rtrouted.log
-        #{var}/log/rbus/rtrouted.err
-    EOS
-  end
-
-  test do
-    # Basic test to verify rbuscli binary
-    system "#{bin}/rbuscli", "--version"
-  end
-end
+   desc "RDK-Bus (RBUS) messaging framework"
+   homepage "https://github.com/rdkcentral/rbus"
+   url "https://github.com/stepherg/rbus/archive/47c405dc4aea747a7af1e568586420e8aa5510dd.tar.gz"
+   version "2.3.1"
+   sha256 "413adc6d8e757e883285040c479e628348a6e371fb0611460f6b748c92b7d776"
+   license "Apache-2.0"
+ 
+   depends_on "cmake" => :build
+   depends_on "cjson"
+   depends_on "msgpack-c"
+   depends_on "stepherg/tap/linenoise"
+ 
+   def install
+     # Configure CMake with Homebrew dependencies
+     args = std_cmake_args + %W[
+       -DBUILD_FOR_DESKTOP=OFF
+       -DBUILD_RBUS_DAEMON=ON
+       -DBUILD_RBUS_SAMPLE_APPS=OFF
+       -DBUILD_RBUS_TEST_APPS=OFF
+       -DBUILD_ONLY_RTMESSAGE=OFF
+       -DENABLE_RDKLOGGER=OFF
+       -DRDKC_BUILD=OFF
+       -DWITH_SPAKE2=OFF
+       -DMSG_ROUNDTRIP_TIME=OFF
+       -DENABLE_UNIT_TESTING=OFF
+       -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+     ]
+     mkdir "build" do
+       system "cmake", "..", *args
+       system "make"
+       system "make", "install"
+     end
+     # Create log and run directories
+     (var/"run/rbus").mkpath
+   end
+ 
+   service do
+     run [opt_bin/"rtrouted"]
+     run_type :immediate
+     keep_alive false
+     process_type :background
+     # Add PID file to track the daemon process
+     pid_file var/"run/rbus/rtrouted.pid"
+   end
+ 
+   def caveats
+     <<~EOS
+       To start rbus now and restart at login:
+         brew services start rbus
+       Or, if you don't want/need a background service, you can run:
+         #{opt_bin}/rtrouted
+  
+       PID file is written to:
+         #{var}/run/rbus/rtrouted.pid
+ 
+       If the service fails to stop properly, you can manually terminate it with:
+         kill -TERM $(cat #{var}/run/rbus/rtrouted.pid)
+     EOS
+   end
+ 
+   test do
+     # Basic test to verify rbuscli binary
+     system "#{bin}/rbuscli", "--version"
+   end
+ end
