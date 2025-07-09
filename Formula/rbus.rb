ba4ef49adf74060f44db094ec7e74ca348fc6824
Robert Stephens <robert64@bellsouth.net>
2025-07-09 15:41:23 -0400

    Update rbus compiler flags

--- Formula/rbus.rb
+++ Formula/rbus.rb
@@ -2,7 +2,7 @@ class Rbus < Formula
   desc "RDK-Bus (RBUS) messaging framework"
   homepage "https://github.com/rdkcentral/rbus"
   url "https://github.com/stepherg/rbus/archive/47c405dc4aea747a7af1e568586420e8aa5510dd.tar.gz"
-  version "2.3.2"
+  version "2.3.3"
   sha256 "413adc6d8e757e883285040c479e628348a6e371fb0611460f6b748c92b7d776"
   license "Apache-2.0"
  
@@ -30,7 +30,7 @@ class Rbus < Formula
      -DENABLE_RDKLOGGER=OFF
      -DRDKC_BUILD=OFF
      -DWITH_SPAKE2=OFF
-     -DMSG_ROUNDTRIP_TIME=OFF
+     -DMSG_ROUNDTRIP_TIME=ON
      -DENABLE_UNIT_TESTING=OFF
      -DCMAKE_POLICY_VERSION_MINIMUM=3.5
     ]
